6246ade7e609677b8f927d099e5b2e51b3289168
Matthew Miller <mnmiller1@me.com>
2020-03-30 22:25:10 +1000

    Added an Overclock cheat

---
+++ src/main/java/com/me4502/racquel/plugin/misc/Overclock.java
@@ -0,0 +1,57 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Madeline Miller
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.me4502.racquel.plugin.misc;
+
+import com.me4502.racquel.mixin.AccessorMinecraftClient;
+import com.me4502.racquel.plugin.Plugin;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.RenderTickCounter;
+import org.lwjgl.glfw.GLFW;
+
+public class Overclock extends Plugin {
+
+    @Override
+    public void init() {
+        super.init();
+    }
+
+    @Override
+    public void enable() {
+        super.enable();
+        ((AccessorMinecraftClient) MinecraftClient.getInstance()).setRenderTickCounter(new RenderTickCounter(20f * 6.67f, 0));
+    }
+
+    @Override
+    public void disable() {
+        super.disable();
+        ((AccessorMinecraftClient) MinecraftClient.getInstance()).setRenderTickCounter(new RenderTickCounter(20, 0));
+    }
+
+    @Override
+    public int getKeyCode() {
+        return GLFW.GLFW_KEY_O;
+    }
+
+}
