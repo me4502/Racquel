f90f998474be50adcbd7fc8b7b10a674d0cebf43
Madeline Miller <mnmiller1@me.com>
2022-06-11 22:07:43 +1000

    Update to Minecraft 1.19

--- src/main/java/com/me4502/racquel/mixin/AccessorMinecraft.java
+++ src/main/java/com/me4502/racquel/mixin/AccessorMinecraft.java
@@ -27,11 +27,13 @@ package com.me4502.racquel.mixin;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.Timer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(Minecraft.class)
 public interface AccessorMinecraft {
 
+    @Mutable
     @Accessor("timer")
     void setTimer(Timer timer);
 
